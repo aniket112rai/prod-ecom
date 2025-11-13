72fb4af65baf1809b6a8206600a78310aea0e99c
ANIKET RAI <raianiket112@gmail.com>
2025-11-14 02:04:05 +0530

    cookie modifications

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -10,7 +10,8 @@ export const sendToken = (res, user) => {
   const token = createToken(user);
   res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "none",
+      secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, 
     })
 
