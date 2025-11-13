3c3eefa4e63711038c70a0b6bb93aada1ca7bc8e
ANIKET RAI <raianiket112@gmail.com>
2025-11-14 01:49:45 +0530

    cors update

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://prod-ecom-frontend.onrender.com",
     credentials: true,
   }));
 app.use(express.json());
